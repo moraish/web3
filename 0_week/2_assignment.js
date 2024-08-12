ce3f42cfc82b8dd5cb4bc4e396cb7d6137c5d4d7
Moraish Kapoor <moraish.kapoor@hotmail.com>
2024-08-12 16:50:03 -0400

    assignment 2

--- 0_week/2_assignment.js
+++ 0_week/2_assignment.js
@@ -1,2 +1,17 @@
+// same problem with a prefix
 const crypto = require('crypto');
 
+
+let i = 1000;
+let output;
+while (i < 100000) {
+    output = crypto.createHash('sha256').update('abcd' + i.toString()).digest('hex');
+
+    if (output.substring(0, 4) == '0000') {
+        console.log(output);
+        console.log(i);
+        break;
+    }
+    // console.log(output.substring(0, 4));
+    i = i + 1;
+}
