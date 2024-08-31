12d50ce9d206ab961e78a20ede9ea1c441d7f4f4
Moraish Kapoor <moraish.kapoor@hotmail.com>
2024-08-31 01:14:15 -0400

    week-2

--- 2_week/learn.js
+++ 2_week/learn.js
@@ -2,3 +2,7 @@ let str = 'h';
 
 const binaryRepresentatiob = new TextEncoder().encode(str);
 console.log(binaryRepresentatiob);
+
+const publicKey = '34xp4vRoCGJym3xR7yCVPFHoCNxv4Twseo'
+const encryptedKey = new TextEncoder().encode(publicKey);
+console.log(encryptedKey);
