8caaa76d79af56d4f6d549cee2d4940485d6ec9e
Moraish Kapoor <moraish.kapoor@hotmail.com>
2025-06-10 20:35:22 -0400

    old updates

---
+++ 3_week/wallet/src/App.tsx
@@ -0,0 +1,16 @@
+import AppBar from "./components/AppBar"
+import SideBar from "./components/SideBar"
+import Footer from "./components/Footer"
+
+function App() {
+
+  return (
+    <>
+      <AppBar />
+      <SideBar />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ 3_week/wallet/src/components/AppBar.tsx
@@ -0,0 +1,7 @@
+export default function AppBar() {
+    return (
+        <div>
+            <h1>AppBar</h1>
+        </div>
+    )
+}
